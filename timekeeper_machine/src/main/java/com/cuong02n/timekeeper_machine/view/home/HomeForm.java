9be75eb3a77275db7f74ad92a88434c664b2ca05
Đăng Đức <95329674+BD2020452915102002@users.noreply.github.com>
2023-12-07 14:00:23 +0700

    kkk

---
+++ timekeeper_machine/src/main/java/com/cuong02n/timekeeper_machine/view/home/HomeForm.java
@@ -0,0 +1,4 @@
+package com.cuong02n.timekeeper_machine.view.home;
+
+public class HomeForm {
+}
---
+++ timekeeper_machine/src/main/java/com/cuong02n/timekeeper_machine/view/login/LoginForm.java
@@ -0,0 +1,12 @@
+package com.cuong02n.timekeeper_machine.view.login;
+
+import javax.swing.*;
+
+public class LoginForm {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JButton btnLogin;
+    private JLabel loginLabel;
+
+}
