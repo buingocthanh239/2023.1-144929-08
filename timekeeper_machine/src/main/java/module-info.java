5777d12b2c57120c84acc4932d9aa60b173700c2
cuong02n <cuong02n@gmail.com>
2023-12-08 13:27:38 +0700

    Add module for swing

--- timekeeper_machine/src/main/java/module-info.java
+++ timekeeper_machine/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.cuong02n.timekeeper_machine {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.cuong02n.timekeeper_machine to javafx.fxml;
