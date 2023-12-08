258d60c1711832d3a20d6c3c8ef8749d2665180b
Đăng Đức <95329674+BD2020452915102002@users.noreply.github.com>
2023-12-08 18:11:48 +0700

    Done loginForm

--- timekeeper_machine/src/main/java/com/cuong02n/timekeeper_machine/view/timekeepingOfWorkersByMonth/timekeepingOfWorkersByDay/submitAComplaint/submitAComplaintForm.java
+++
@@ -1,9 +0,0 @@
-package com.cuong02n.timekeeper_machine.view.timekeepingOfWorkersByMonth.timekeepingOfWorkersByDay.submitAComplaint;
-
-import javax.swing.*;
-
-public class submitAComplaintForm {
-    private JTextPane textPane1;
-    private JButton submitButton;
-
-}
--- timekeeper_machine/src/main/java/com/cuong02n/timekeeper_machine/view/timekeepingStatistics/showDetail/showDetailWorker.java
+++
@@ -1,8 +0,0 @@
-package com.cuong02n.timekeeper_machine.view.timekeepingStatistics.showDetail;
-
-import javax.swing.*;
-
-public class showDetailWorker {
-    private JTable table1;
-    private JScrollBar scrollBar1;
-}
