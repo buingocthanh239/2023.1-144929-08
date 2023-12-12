c62d26ae32438f336fbf40150ef0d6a4122a27d9
Đăng Đức <95329674+BD2020452915102002@users.noreply.github.com>
2023-12-12 17:59:14 +0700

    ;-;

--- timekeeper_machine/src/main/java/com/cuong02n/timekeeper_machine/HelloApplication.java
+++ timekeeper_machine/src/main/java/com/cuong02n/timekeeper_machine/HelloApplication.java
@@ -2,19 +2,28 @@ package com.cuong02n.timekeeper_machine;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private static Stage stg;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
+        stg = stage;
+        stage.setResizable(false);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("loginForm.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Phần mềm quản lý chấm công");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void changeScene(String fxml) throws IOException {
+
     }
 
     public static void main(String[] args) {
--- timekeeper_machine/src/main/java/com/cuong02n/timekeeper_machine/HelloController.java
+++ timekeeper_machine/src/main/java/com/cuong02n/timekeeper_machine/HelloController.java
@@ -3,12 +3,15 @@ package com.cuong02n.timekeeper_machine;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import java.awt.event.ActionEvent;
+
 public class HelloController {
     @FXML
     private Label welcomeText;
 
+
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void onLoginButtonClick(ActionEvent event) {
+
     }
 }
